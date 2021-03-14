a0881ccdca878edf5e2e7b00b1d477b21690c453
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-03-13 21:15:53 -0500

    Added proper train and test dataloaders

--- Classifier.py
+++ Classifier.py
@@ -1,13 +1,19 @@
 from Utils import Utils
 import sys
+import torch
 
 
 def main():
     dataroot = "C:/Users/anast/Documents/_Spring_2021/Senior_Project/dataset"
     #--------------prepare data------------------------
     utils = Utils()
-    dataloader = utils.prepare_data(dataroot, image_size, batch_size, workers)
-    utils.plot_training_images(dataloader, device)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    workers = 1
+    batch_size = 16
+    image_size = 128
+    trainloader, testloader = utils.prepare_data(dataroot, image_size, batch_size)
+    utils.plot_images(trainloader, device, 'training_images')
+    utils.plot_images(testloader, device, 'testing_images')
 
 
 if __name__ == "__main__":
--- Utils.py
+++ Utils.py
@@ -6,25 +6,40 @@ import numpy as np
 import torchvision.utils as vutils
 
 class Utils(object):
-    def prepare_data(self, dataroot, image_size, batch_size, workers):
+    def prepare_data(self, dataroot, image_size, batch_size):
         # Create the dataset
-        dataset = dset.ImageFolder(root=dataroot,
+        print("Preparing dataset...")
+        train_dataset = dset.ImageFolder(root=dataroot+'/Train',
                                    transform=transforms.Compose([
                                        transforms.Resize(image_size),
                                        transforms.CenterCrop(image_size),
                                        transforms.ToTensor(),
                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                    ]))
+        print(train_dataset.classes)
         # Create the dataloader
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                                 shuffle=True, num_workers=workers)
-        return dataloader
+        test_dataset = dset.ImageFolder(root=dataroot+'/Test',
+                                   transform=transforms.Compose([
+                                       transforms.Resize(image_size),
+                                       transforms.CenterCrop(image_size),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                   ]))
+        print(test_dataset.classes)
 
-    def plot_training_images(self, dataloader, device):
+        print("Preparing dataloaders...")
+        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                                 shuffle=True)
+        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size//2,
+                                                 shuffle=True)
+        return trainloader, testloader
+
+    def plot_images(self, dataloader, device, plot_title):
         # Plot some training images
+        print(f"Plotting some {plot_title}")
         real_batch = next(iter(dataloader))
         plt.figure(figsize=(8,8))
         plt.axis("off")
-        plt.title("Training Images")
-        plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+        plt.title(plot_title)
+        plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:16], padding=2, normalize=True).cpu(),(1,2,0)))
         plt.show()
